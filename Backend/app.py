6b05c0a2b041b11a560bd64fa29b1e705d32306f
Dhayaalan <dhayaalanjayakumar2001@gmail.com>
2023-08-25 16:29:45 +0530

    Post Method for Node and Links

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+# import json
 import pymongo
 
 app = Flask(__name__)
@@ -15,17 +16,66 @@ db = client[database_name]
 nodes_collection = db[collection_name_nodes]
 links_collection = db[collection_name_links]
 
+
+# class CustomJSONEncoder(json.JSONEncoder):
+#     def default(self, o):
+#         if isinstance(o, ObjectId):
+#             return str(o)
+#         return super().default(o)
+
+
+# app.json_encoder = CustomJSONEncoder
+
+
+# @app.route('/add_node', methods=['POST'])
+# def add_node():
+#     data = request.json
+#     id = data['id']
+#     group = data['group']
+#     label = data['label']
+#     level = data['level']
+#     # print(node, 'result')
+
+#     new_data = {
+#         'id': id,
+#         'group': group,
+#         'label': label,
+#         'level': level
+#     }
+#     print(new_data)
+
+#     nodes_collection.insert_one(new_data)
+#     return jsonify({"message": "Node added successfully", "new_data": new_data}), 200
+
+
 @app.route('/add_node', methods=['POST'])
 def add_node():
     data = request.json
-    nodes_collection.insert_one(data)
-    return jsonify({"message": "Node added successfully",'data': data})
+    id = data['id']
+    group = data['group']
+    label = data['label']
+    level = data['level']
+
+    new_data = {
+        'id': id,
+        'group': group,
+        'label': label,
+        'level': level
+    }
+
+    nodes_collection.insert_one(new_data)
+
+    new_data['_id'] = str(new_data['_id'])
+    return jsonify({"message": "Node added successfully", "new_data": new_data}), 200
+
+
 
 @app.route('/add_link', methods=['POST'])
 def add_link():
     data = request.json
     links_collection.insert_one(data)
-    return jsonify({"message": "Link added successfully",'data':data})
+    return jsonify({"message": "Link added successfully", 'data': data})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
