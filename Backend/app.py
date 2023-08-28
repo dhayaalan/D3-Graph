2fc35731490e95874438738b76a38907612bcd2d
Dhayaalan <dhayaalanjayakumar2001@gmail.com>
2023-08-28 11:37:48 +0530

    Backend works for nodes and links has been completed

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import flask_cors
 # import json
 import pymongo
 
+
 app = Flask(__name__)
+CORS(app)
+
+cors_ip = '*'
+
 
 # MongoDB connection details
 mongo_uri = "mongodb://localhost:27017/"
@@ -49,55 +56,64 @@ links_collection = db[collection_name_links]
 
 
 @app.route('/add_node', methods=['POST'])
+@flask_cors.cross_origin(origin=cors_ip, support_credentials=True)
 def add_node():
     data = request.json
+    print(data, "data")
     id = data['id']
     group = data['group']
     label = data['label']
     level = data['level']
 
-    new_data = {
+    new_node = {
         'id': id,
         'group': group,
         'label': label,
         'level': level
     }
+    # print(new_node)
+    nodes_collection.insert_one(new_node)
 
-    nodes_collection.insert_one(new_data)
-
-    new_data['_id'] = str(new_data['_id'])
-    return jsonify({"message": "Node added successfully", "new_data": new_data}), 200
+    new_node['_id'] = str(new_node['_id'])
+    node = [new_node]
+    print(node, 'new node')
+    return jsonify({"message": "Node added successfully", "new_data": node}), 200
 
 
 @app.route('/add_link', methods=['POST'])
+@flask_cors.cross_origin(origin=cors_ip, support_credentials=True)
 def add_link():
     data = request.json
     target = data['target']
     source = data['source']
     strength = data['strength']
 
-    new_data = {
+    new_link = {
         "target": target,
         "source": source,
         "strength": strength
     }
 
-    new_data['_id'] = str(new_data['_id'])
-    links_collection.insert_one(data)
-    return jsonify({"message": "Link added successfully", 'new_data': new_data}), 200
+    links_collection.insert_one(new_link)
+
+    new_link['_id'] = str(new_link['_id'])
+    link = [new_link]
+    return jsonify({"message": "Link added successfully", 'new_data': link}), 200
 
 
 @app.route('/get_nodes', methods=['GET'])
+@flask_cors.cross_origin(origin=cors_ip, support_credentials=True)
 def get_nodes():
-    nodes = list(nodes_collection.find({}, {'_id': 0}))
+    nodes = list(nodes_collection.find([], {'_id': 0}))
     return jsonify({"nodes": nodes}), 200
 
 
 @app.route('/get_links', methods=['GET'])
+@flask_cors.cross_origin(origin=cors_ip, support_credentials=True)
 def get_links():
     links = list(links_collection.find({}, {'_id': 0}))
     return jsonify({"links": links}), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=4000, host='0.0.0.0')
